8a9e4d8679a3516ce3df2e073c472c0b602a5a9b
Gustavo Amorim <gustavoamorim02@gmail.com>
2017-03-22 15:54:05 -0300

    Update SMSReceiver

--- app/src/main/java/br/com/amorimgc/receiver/SMSReceiver.java
+++ app/src/main/java/br/com/amorimgc/receiver/SMSReceiver.java
@@ -3,14 +3,41 @@ package br.com.amorimgc.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.List;
+
+import br.com.amorimgc.agenda.R;
+import br.com.amorimgc.dao.AlunoDAO;
+import br.com.amorimgc.model.Aluno;
 
 /**
  * Created by Gustavo Amorim on 17/03/2017.
+ * Pourpose: Alert specificly when receive a student's sms message.
  * @author amorimgc
  */
 public class SMSReceiver extends BroadcastReceiver{
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: IN PROGRESS
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        SmsMessage sms = SmsMessage.createFromPdu(pdu, intent.getSerializableExtra("format").toString());
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDAO dao = new AlunoDAO(context);
+
+        if (dao.isAluno(telefone)) {
+            Toast.makeText(context, "Chegou um SMS de um Aluno", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+
+        dao.close();
     }
 }
